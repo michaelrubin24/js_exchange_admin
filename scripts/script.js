72c0fc02967e251807420a1fda5f38aacdb84929
Michael Rubinshtein <deliverypoint7@yandex.ru>
2017-10-28 18:40:38 +0300

    edit text in alert

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 
 var select = prompt(" Какую программу запустить?"+ "\n" + "1. Курс валют (нажмите 1)" + "\n" + "2. Вход в админку (нажмите 2)");
 
-if(select == 1){ // Курс валют
+if (select == 1){ // Курс валют
 
   var dollar = 26, evro = 31, rubl = 0.395;
   var grn = prompt("Сколько гривен вы бы хотели поменять?");
 
-  if(isNaN(grn)){
+  if (isNaN(grn)){
     alert("Введите число!");
   }
   else{
@@ -16,47 +16,47 @@ if(select == 1){ // Курс валют
     alert("Вы можете получить " + grn_in_dollar + " долларов, " + "или " + grn_in_evro + " в евро, или " + grn_in_rubl + " рублей");
     var money = prompt("В какой валюте хотите получить деньги?");
 
-    if(money == "$" || money == "dollar" || money == "доллар"){
+    if (money == "$" || money == "dollar" || money == "доллар"){
         var answer = confirm("Вы действительно хотите поменять " + grn + " гривен, на " + grn_in_dollar + " долларов?");
-        if(answer != true){
+        if (answer != true){
           alert("Приходите в другой раз");
-        }else{
+        }else {
           alert("спасибо");
         }
-    } else if(money == "€" || money == "evro" || money == "евро"){
+    } else if (money == "€" || money == "evro" || money == "евро"){
         var answer = confirm("Вы действительно хотите поменять " + grn + " гривен, на " + grn_in_evro + " евро?");
-        if(answer != true){
+        if (answer != true){
           alert("Приходите в другой раз");
-        }else{
+        }else {
           alert("спасибо");
         }
-    } else if(money == "'₽" || money == "rub" || money == "рубль"){
+    } else if (money == "'₽" || money == "rub" || money == "рубль"){
         var answer = confirm("Вы действительно хотите поменять " + grn + " гривен, на " + grn_in_rubl + " рублей?");
-        if(answer != true){
+        if (answer != true){
           alert("Приходите в другой раз");
-        }else{
+        }else {
           alert("спасибо");
         }
       }
-      else{
+      else {
         alert("Неправильный ввод");
       }
   }
-}else if(select == 2){ //Вход в админку
+}else if (select == 2){ //Вход в админку
   var login = "admin";
   var password = "nice";
   var login_check = prompt("Введите логин:");
-  if(login == login_check){
+  if (login == login_check){
     var password_check = prompt("Введите пароль:");
-    if(password == password_check){
+    if (password == password_check){
       alert("Привет админ!");
-    }else{
+    }else {
       alert("Неправильный пароль!");
-        }
-  }else{
+          }
+  }else {
     alert("Неправильный логин!");
   }
 
-}else{
+}else {
   alert("Неправильный ввод");
 }
